8c22c65eda9ea69d650d45a8f0b76093833880b6
OldRoldGoldenfold <strontiem@gmail.com>
2015-01-27 00:04:02 -0500

    Old Rold...At it again

--- Rold_LinkedList/LinkedList.java
+++ Rold_LinkedList/LinkedList.java
@@ -8,229 +8,95 @@ import java.util.Iterator;
  */
 public class LinkedList implements Iterable<Object> {
 	private ListNode head;
-	private int size;
-
-	/**
-	 * adds an object to the end of the LinkedList
-	 * @param o <code>Object</code> to be added
-	 */
-	public void append(Object o) {
-		size++;
-		ListNode n = new ListNode(o);
-		ListNode temp = head.getPrev();
-		temp.setNext(n);
-		head.setPrev(n);
-		n.setNext(head);
-		n.setPrev(temp);
-	}
-
-	/**
-	 * Constructs an empty LinkedList
-	 */
-	public LinkedList() {
-		head = null;
-		size = 0;
-	}
-
-	/**
-	 * Constructs a LinkedList with a single Item
-	 * @param o <code>Object</code> to be added
-	 */
-	public LinkedList(Object o) {
-		size = 1;
-		head = new ListNode(o, head, head);
-	}
-
-	/**
-	 * Constructs a LinkedList from an Array of Objects
-	 * @param o <code>Object[]</code> to be added
-	 */
-	public LinkedList(Object[] o) {
-		size = o.length;
-		head = new ListNode(o[0], head, head);
-		head.setNext(getNextNode(o, head, 1));
-	}
-
-	/**
-	 * Used by LinkedList(<code>Object[]</code>)
-	 */
-	private ListNode getNextNode(Object[] o, ListNode n, int i) {
-		ListNode temp = (i < o.length) ? new ListNode(o[i]) : head;
-		if (temp != head) {
-			temp.setPrev(n);
-			temp.setNext(getNextNode(o, temp, ++i));
-		} else {
-			head.setPrev(n);
+	private int size = 0;
+	
+	public LinkedList()					{													}
+	public LinkedList(KeyValuePair key) {	add(key);										}
+	public LinkedList(KeyValuePair[] k)	{	for(KeyValuePair key : k)	add(key);			}
+	public int getSize()				{	return size;									}
+	public KeyValuePair getHead()		{	return (head == null)? null : head.getValue();	}
+	public Iterator<Object> iterator() 	{	return new ListNodeIterator(head);				}
+	
+	
+	public void add(KeyValuePair k){
+		ListNode in = new ListNode(k);
+		switch (++size){
+		case 1:	head = in;
+				return;
+		case 2:	head.setNext(in);
+				head.setPrev(in);
+				in.setNext(head);
+				in.setPrev(head);
+				head = (head.compareTo(in) < 0)? head : in;
+				return;
 		}
-		return temp;
-	}
-
-	/**
-	 * Returns each Object in the list's toString() with line breaks between
-	 */
-	public String toString() {
-		if (head == null) {
-			return "";
+		ListNode temp = head;
+		for (int i = 1; i < size; i++)	{
+			if (temp.compareTo(in) < 0)	temp = temp.getNext();
+			else {
+				insert(in, temp);
+				head = (head == temp)? in : head;
+				return;
+			}
 		}
-		String out = (head.getValue().toString());
+		insert(in, head);
+	}
+	
+	
+	private void insert(ListNode in, ListNode next)	{
+		ListNode beforeNext = next.getPrev();
+		beforeNext.setNext(in);
+		in.setPrev(beforeNext);
+		in.setNext(next);
+		next.setPrev(in);
+	}
+	
+	public void removeAll(KeyValuePair k){	while (remove(k)){}	}
+	public boolean remove(KeyValuePair k){
+		switch (size)	{
+		case 0: return false;
+		case 1: head = (head.getValue().compareTo(k) == 0)? null : head;
+				size -= (head == null)? 1 : 0;
+				return (head == null)? true : false;
+		}
+		ListNode temp = head;
+		if	(head.getValue().compareTo(k) == 0)	{
+			head = head.getNext();
+			delete(temp);
+			size--;
+			return true;
+		}
+		else	{
+			temp = temp.getNext();
+			while (temp != head)	{
+				if ( temp.getValue().compareTo(k) == 0){
+					delete(temp);
+					size--;
+					return true;
+				}
+				temp = temp.getNext();
+			}
+		}
+		return false;
+	}
+	
+	
+	private void delete(ListNode tbd)	{
+		ListNode prev = tbd.getPrev();
+		ListNode next = tbd.getNext();
+		prev.setNext(next);
+		next.setPrev(prev);
+	}
+	
+	
+	public String toString()	{
+		String out = "";
+		out += (head == null)? "" : head.toString();
 		ListNode temp = head.getNext();
-		while (temp != head) {
-			out += "\n" + temp.getValue().toString();
+		while (temp != head)	{ 
+			out += "\n" + temp.toString();
 			temp = temp.getNext();
 		}
 		return out;
 	}
-
-	/**
-	 * returns the first Node in the List
-	 * @return <code>ListNode</code> head
-	 */
-	public ListNode getHead() {
-		return head;
-	}
-
-	/**
-	 * returns the size of the list
-	 * @return <code>int</code> size
-	 */
-	public int getSize() {	return size;	}
-
-	/**
-	 * Implements the Iterator Interface
-	 */
-	public Iterator<Object> iterator() {	return new ListNodeIterator(head);	}
-
-	/**
-	 * If the item is present, removes the first instance and returns true.
-	 * Returns false if the Object is not present.
-	 * 
-	 * @param o <code>Object</code> to be removed
-	 * @return
-	 */
-	public boolean removeObject(Object o) {
-		ListNode temp = head;
-		if (size == 0)
-			return false;
-		if (head.getValue().equals(o)) {
-			delete(head);
-			size--;
-			return true;
-		}
-		temp = head.getNext();
-		while (temp != head) {
-			if (temp.getValue().equals(o)) {
-				delete(temp);
-				size--;
-				return true;
-			}
-			temp = temp.getNext();
-		}
-		return false;
-	}
-
-	private void delete(ListNode n) {
-		if (size == 1) {
-			head = null;
-			return;
-		}
-		if (n == head) {
-			head = n.getNext();
-		}
-		ListNode prev = n.getPrev();
-		ListNode next = n.getNext();
-		prev.setNext(next);
-		next.setPrev(prev);
-
-	}
-
-	/**
-	 * If an Object is present, removes all instances and returns true. If the
-	 * Object is not present, returns false.
-	 * @param o <code>Object</code> to be eliminated
-	 * @return 
-	 */
-	public boolean removeAllObject(Object o) {
-		boolean contains = false;
-		while (removeObject(o)) {
-			contains = true;
-		}
-		return contains;
-	}
-
-	/**
-	 * Adds an array of Objects to the end of the LinkedList
-	 * 
-	 * @param o <code>Object[]</code> to be deleted
-	 */
-	public void append(Object[] o) {
-		LinkedList temp = new LinkedList(o);
-		if (head == null)
-			head = temp.getHead();
-		else {
-			ListNode oldTail = getTail();
-			ListNode tempHead = temp.getHead();
-			ListNode newTail = temp.getTail();
-			oldTail.setNext(tempHead);
-			tempHead.setPrev(oldTail);
-			newTail.setNext(head);
-			head.setPrev(newTail);
-		}
-		size += temp.getSize();
-	}
-
-	/**
-	 * returns the last node in the list.
-	 * 
-	 * @return
-	 */
-	public ListNode getTail() {
-		return head.getPrev();
-	}
-
-	/**
-	 * Adds a node to the end of a List.
-	 * @param n <code>ListNode</code> to be added
-	 */
-	public void append(ListNode n) {
-		if (head == null)
-			head = n;
-		else {
-			ListNode prev = getTail();
-			prev.setNext(n);
-			n.setPrev(prev);
-			head.setPrev(n);
-			n.setNext(head);
-		}
-		size += 1;
-	}
-	
-	/**
-	 * finds the first index of the <code>Object</code>
-	 * @param o <code>Object</code>
-	 * @return first index of o, -1 if not found
-	 */
-	public int indexOf(Object o){
-		ListNode temp = head;
-		for (int i = 0; i < size; i++){
-			if (temp.getValue().equals(o)) return i;
-			temp = temp.getNext();
-		}
-		return -1;
-	}
-
-	/**
-	 * returns the object stored at a certain
-	 * @param o <code>int</code> index
-	 * @return <code>Object</code>
-	 */
-	public Object get(int o){
-		ListNode temp = head;
-		for (int i = 0; i < size; i++)	{
-			if (i == o) return temp.getValue();
-			temp = temp.getNext();
-		}
-		System.err.println("Item Not Found!");
-		return null;
-	}
-
 }
--- Rold_LinkedList/ListNode.java
+++ Rold_LinkedList/ListNode.java
@@ -3,32 +3,37 @@
  * @author OldRoldGoldenfold
  * @version 01/23/2015
  */
-public class ListNode {
-	private Object value;
+public class ListNode implements Comparable<ListNode>{
+	private KeyValuePair value;
 	private ListNode next;
 	private ListNode prev;
 	
-	public ListNode(Object v, ListNode p, ListNode n)	{
+	public ListNode(KeyValuePair v, ListNode p, ListNode n)	{
 		value = v;
 		next = n;
 		prev = p;
 	}
-	public ListNode(Object v, ListNode p){
+	public ListNode(KeyValuePair v, ListNode p){
 		value = v;
 		next = null;
 		prev = p;
 	}
-	public ListNode(Object v){
+	public ListNode(KeyValuePair v){
 		value = v;
 		next = null;
 		prev = null;
 	}
-	public Object getValue()	{	return value;	}
+	public KeyValuePair getValue()	{	return value;	}
 	public ListNode getNext()	{	return next;	}
 	public ListNode getPrev()	{	return prev;	}
-	public void setValue(Object v)	{ value = v;	}
+	public void setValue(KeyValuePair v)	{ value = v;	}
 	public void setNext(ListNode n)	{ next = n;	}
 	public void setPrev(ListNode p)	{ prev = p;	}
+	
+	public String toString()	{	return value.toString();	}
+	
+	public int compareTo(ListNode other) {	return this.getValue().compareTo(other.getValue());	}
+	
 	
 	
 	
